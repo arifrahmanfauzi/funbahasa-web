948e40072112788fdcbb8fcb1c577642fc5d2955
Spyder-code <luaysyauqillah@gmail.com>
2021-02-03 11:56:29 +0700

    Add api User

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home',  'HomeController@index')->name('home');
 
 // Login Google
-Route::get('login/google',[App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle'])->name('login.google');
-Route::get('login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
+Route::get('login/google', 'Auth\LoginController@redirectToGoogle')->name('login.google');
+Route::get('login/google/callback',  'Auth\LoginController@handleGoogleCallback');
 
 // login facebook
-Route::get('login/facebook',[App\Http\Controllers\Auth\LoginController::class, 'redirectToFacebook'])->name('login.facebook');
-Route::get('login/facebook/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleFacebookCallback']);
+Route::get('login/facebook', 'Auth\LoginController@redirectToFacebook')->name('login.facebook');
+Route::get('login/facebook/callback',  'Auth\LoginController@handleFacebookCallback');
